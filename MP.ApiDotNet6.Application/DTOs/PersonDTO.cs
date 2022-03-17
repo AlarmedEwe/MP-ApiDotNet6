5180cb872dadee38086edef02c1b9b3967b5b429
Guilherme Martinho Luiz <guilherme11do9@gmail.com>
2022-03-16 23:05:41 -0300

    Person DTO

---
+++ MP.ApiDotNet6.Application/DTOs/PersonDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MP.ApiDotNet6.Application.DTOs;
+public class PersonDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Docuent { get; set; }
+    public string Phone { get; set; }
+}
---
+++ MP.ApiDotNet6.Application/DTOs/Validations/PersonDTOValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace MP.ApiDotNet6.Application.DTOs.Validations;
+public class PersonDTOValidator : AbstractValidator<PersonDTO>
+{
+    public PersonDTOValidator()
+    {
+        RuleFor(x => x.Docuent)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Documento deve ser informado!");
+
+        RuleFor(x => x.Name)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Nome deve ser informado!");
+
+        RuleFor(x => x.Phone)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Celular deve ser informado!");
+    }
+}
