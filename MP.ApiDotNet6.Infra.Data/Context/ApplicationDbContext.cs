ae099beae3869497c7100c2a1e3a7c3f7d6b37be
Guilherme Martinho Luiz <guilherme11do9@gmail.com>
2022-03-16 22:59:46 -0300

    Creating repositories for person and product

--- MP.ApiDotNet6.Infra.Data/Context/ApplicationDbContext.cs
+++ MP.ApiDotNet6.Infra.Data/Context/ApplicationDbContext.cs
@@ -2,11 +2,13 @@
 using MP.ApiDotNet6.Domain.Entities;
 
 namespace MP.ApiDotNet6.Infra.Data.Context;
-internal class ApplicationDbContext : DbContext
+public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
     public DbSet<Person> People { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Purchase> Purchases { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
